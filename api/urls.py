89b7af8091ac4c7144f735edd9cbbbc4429a7b74
MGubernick <MGubernick61@gmail.com>
2021-02-17 17:32:07 -0600

    Update to urls.py
    - Updated the urls.py document so that all of the urls had the trailing
    slash

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,9 @@ from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 urlpatterns = [
   	# Restful routing
     # Workout Views
-    path('workouts', Workouts.as_view(), name='workouts'),
-    path('workouts/<int:pk>', WorkoutDetail.as_view(), name='workout_detail'),
-    path('myworkouts', MyWorkouts.as_view(), name='myworkouts'),
+    path('workouts/', Workouts.as_view(), name='workouts'),
+    path('workouts/<int:pk>/', WorkoutDetail.as_view(), name='workout_detail'),
+    path('myworkouts/', MyWorkouts.as_view(), name='myworkouts'),
     # Auth Paths
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
